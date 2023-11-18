d43e7b81606556713736d68134dddf40622ed1e2
Satyam <90526991+satyam9416@users.noreply.github.com>
2023-11-18 22:45:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,35 @@ const express = require("express");
 const app = express()
 const path = require("path");
 const PORT = process.env.PORT || 3000;
+import { Server } from 'socket.io'
+import http from 'http'
 
+const server = http.createServer(app)
 // express specific stuff 
 app.use("/static", express.static('static'));
 
 // app.set('views', path.join(__dirname + 'views'));
 
-// app.set('view engine', 'html');
+const io = new Server(server)
 
+const users = {};
+
+io.on('connection', socket =>{
+    console.log("connected....");
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);;
+    });
+
+    socket.on('msg-sent', message => {
+        socket.broadcast.emit('recieve', {message: message, name:users[socket.id]});
+    });
+
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log("client-server is running on the port " + PORT + "...")
 })
